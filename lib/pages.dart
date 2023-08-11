dfe2743ecc79e157435a9cec8082df97fabad694
PhoenixDev1223 <phoenixdev1223@gmail.com>
2023-08-11 17:31:12 +0900

    update

---
+++ lib/pages.dart
@@ -0,0 +1,21 @@
+export 'presentation/pages/app_base_page.dart';
+
+export 'presentation/pages/auth/login_page.dart';
+
+export 'presentation/pages/home_page.dart';
+export 'presentation/pages/setting_page.dart';
+
+export 'presentation/pages/account/account_page.dart';
+export 'presentation/pages/account/avatar_dialog_page.dart';
+export 'presentation/pages/account/dob_page.dart';
+export 'presentation/pages/account/username_page.dart';
+export 'presentation/pages/account/gender_page.dart';
+export 'presentation/pages/account/appearance_page.dart';
+export 'presentation/pages/account/password_page.dart';
+
+export 'presentation/pages/help_support/help_support_page.dart';
+
+export 'presentation/pages/path_finder/path_finder_page.dart';
+export 'presentation/pages/path_finder/your_answer_page.dart';
+export 'presentation/pages/path_finder/path_finder_report_page.dart';
+
